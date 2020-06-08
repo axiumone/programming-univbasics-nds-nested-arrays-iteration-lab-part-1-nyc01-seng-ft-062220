bb6311ace2770d6858ba4ea0ac1f2fa5bfd447f8
Dmitry Sychev <axiumone@gmail.com>
2020-06-08 14:36:37 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,6 +3,6 @@ def find_even_values(src)
   #
   # Output all even values in each nested array
 
-  src.flatten.select { |num| num % 2 == 0}
-
+  evens = src.flatten.select { |num| num % 2 == 0}
+  evens
 end
